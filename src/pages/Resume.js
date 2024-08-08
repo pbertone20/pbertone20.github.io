79e06f44b6336dfb7f6edea6453e66c267b3651e
Peter Bertone <pbertone@uoguelph.ca>
2024-08-08 13:49:29 -0400

    Switched to Resume to a Google Doc embed...

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import ResumePDF from '../assets/pbertone-resume.pdf';
 
 const Resume = () => {
     return (
         <main className="p-3 bg-base">
             <div className="text-black flex flex-col items-center m-2">
                 <section className='text-box bg-secondary p-5 rounded-xl'>
-            <       iframe
-                        src={ResumePDF}
-                        className="size-full"
-                        style={{ minHeight: '100vh', minWidth: '90vw'}}
+                    <embed
+                        src='https://drive.google.com/file/d/1YL285LoQXcZjqpY5C-QVwupZC1E-Fkr3/preview?usp=sharing'
+                        className="h-[100vh] w-[90vw]"
                         title="Resume"
-                    >
-                        <p>Your browser does not support PDFs. <a href={ResumePDF}>Download the PDF</a>.</p>
-                    </iframe>                
-                </section>
+                    />
+            </section>
             </div>
         </main>
     );
